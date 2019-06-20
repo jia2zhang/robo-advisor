2ecfd11f7614b53d991644ac966affcd66c4939b
Jessica <jessica.jiajia.zhang@gmail.com>
2019-06-19 22:15:02 -0400

    embedded code in else statement

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -26,79 +26,72 @@ while True:
         # print(response.status_code)
         # print(stock_symbol)
         else: 
+            ## Process input
 
+            last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
+            def to_usd(cost):
+                return str("${0:,.2f}".format(cost))
 
-## Process input
+            symbol = parsed_response["Meta Data"]["2. Symbol"]
+            tsd = parsed_response["Time Series (Daily)"]
+            dates = list(tsd.keys()) # TODO: Need to make sure the dates are sorted with the latest first
+            latest_day = dates[0]
+            latest_close = tsd[latest_day]["4. close"]
+            request_time = ""+datetime.datetime.now().strftime("%Y-%m-%d %I:%M%p")
 
-# request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock_symbol}&apikey={api_key}"
-# response = requests.get(request_url)
-# parsed_response = json.loads(response.text)
-# print(response)
+            # Max of all the high prices
+            high_prices = []
+            low_prices = []
 
-last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+            for date in dates:
+                high_price = tsd[latest_day]["2. high"]
+                high_prices.append(float(high_price))
+                low_price = tsd[latest_day]["3. low"]
+                low_prices.append(float(low_price))
 
-def to_usd(cost):
-    return str("${0:,.2f}".format(cost))
+            recent_high = max(high_prices)
+            recent_low = min(low_prices)
 
-symbol = parsed_response["Meta Data"]["2. Symbol"]
-tsd = parsed_response["Time Series (Daily)"]
-dates = list(tsd.keys()) # TODO: Need to make sure the dates are sorted with the latest first
-latest_day = dates[0]
-latest_close = tsd[latest_day]["4. close"]
-request_time = ""+datetime.datetime.now().strftime("%Y-%m-%d %I:%M%p")
+            ## Process output
+            csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices_"+symbol+".csv")
 
-# Max of all the high prices
-high_prices = []
-low_prices = []
+            csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+            with open(csv_file_path, "w") as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames = csv_headers)
+                writer.writeheader()
+                # loop through each row to write each row
 
-for date in dates:
-    high_price = tsd[latest_day]["2. high"]
-    high_prices.append(float(high_price))
-    low_price = tsd[latest_day]["3. low"]
-    low_prices.append(float(low_price))
+                for date in dates:
+                    daily_prices = tsd[date]
+                    writer.writerow({
+                        "timestamp": date, 
+                        "open": daily_prices["1. open"], 
+                        "high": daily_prices["2. high"], 
+                        "low": daily_prices["3. low"], 
+                        "close": daily_prices["4. close"], 
+                        "volume": daily_prices["5. volume"]
+                    })
 
-recent_high = max(high_prices)
-recent_low = min(low_prices)
-
-## Process output
-csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
-
-csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
-with open(csv_file_path, "w") as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames = csv_headers)
-    writer.writeheader()
-    # loop through each row to write each row
-
-    for date in dates:
-        daily_prices = tsd[date]
-        writer.writerow({
-            "timestamp": date, 
-            "open": daily_prices["1. open"], 
-            "high": daily_prices["2. high"], 
-            "low": daily_prices["3. low"], 
-            "close": daily_prices["4. close"], 
-            "volume": daily_prices["5. volumex"]
-        })
-
-print("-------------------------")
-print(f"SELECTED SYMBOL: {symbol}")
-print("-------------------------")
-print("REQUESTING STOCK MARKET DATA")
-print(f"REQUEST AT: {request_time}")
-print("-------------------------")
-print(f"LATEST DAY: {last_refreshed}")
-print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print(f"RECENT HIGH: {to_usd(float(recent_high))}")
-print(f"RECENT LOW: {to_usd(float(recent_low))}")
-print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
-print("-------------------------")
-print(f"WRITING DATA TO CSV: {csv_file_path}...")
-print("-------------------------")
-print("HAPPY INVESTING!")
-print("-------------------------")
+            print("-------------------------")
+            print(f"SELECTED SYMBOL: {symbol}")
+            print("-------------------------")
+            print("REQUESTING STOCK MARKET DATA")
+            print(f"REQUEST AT: {request_time}")
+            print("-------------------------")
+            print(f"LATEST DAY: {last_refreshed}")
+            print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
+            print(f"RECENT HIGH: {to_usd(float(recent_high))}")
+            print(f"RECENT LOW: {to_usd(float(recent_low))}")
+            print("-------------------------")
+            print("RECOMMENDATION: BUY!")
+            print("RECOMMENDATION REASON: TODO")
+            print("-------------------------")
+            print(f"WRITING DATA TO CSV: {csv_file_path}...")
+            print("-------------------------")
+            print("HAPPY INVESTING!")
+            print("-------------------------")
+            break
 
 
 
